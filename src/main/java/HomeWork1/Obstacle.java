d3e66107aa3fe57538b4dec3e7b1b737b27864be
Storrtor <storrtor@gmail.com>
2021-04-27 18:09:20 +0300

    Добавлены абстракт классы и еще интерфесы

--- src/main/java/HomeWork1/Obstacle.java
+++ src/main/java/HomeWork1/Obstacle.java
@@ -4,10 +4,17 @@ abstract class Obstacle {
     private int parameter;
 
     public Obstacle(int parameter) {
+        validation(parameter);
         this.parameter = parameter;
     }
 
     public int getParameter() {
         return parameter;
+    }
+
+    public void validation(int parameter){
+        if (parameter < 0){
+            throw new IllegalArgumentException("Нельзя сделать отрицательное препятствие");
+        }
     }
 }
--- src/main/java/HomeWork1/Wall.java
+++ src/main/java/HomeWork1/Wall.java
@@ -4,4 +4,5 @@ public class Wall extends HighObstacle{
     public Wall(int high) {
         super(high);
     }
+
 }
