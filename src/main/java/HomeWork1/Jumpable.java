7cc491438d75f4ecb085bb3d630ce6038477014d
Storrtor <storrtor@gmail.com>
2021-04-30 17:23:51 +0300

    Закончена дз

--- src/main/java/HomeWork1/Jumpable.java
+++ src/main/java/HomeWork1/Jumpable.java
@@ -4,27 +4,4 @@ public interface Jumpable  {
 
     boolean jump(HighObstacle highObstacle);
 
-
-//    default void jump(){
-//        System.out.println("прыгнул");
-//    }
-//
-//    @Override
-//    default boolean overtake(Obstacle obstacle) {
-//        return jump((HighObstacle) obstacle);
-//    }
-//
-//    default boolean jump(HighObstacle highObstacle){
-//        if (validate(highObstacle) == true){
-//            System.out.println("Перепрыгнул препятствие");
-//            return true;
-//        } else {
-//            System.out.println("Не перепрыгнул препятствие");
-//            return false;
-//        }
-//    }
-//
-//    default boolean validate(HighObstacle obstacle){
-//        return true;
-//    }
 }
--- src/main/java/HomeWork1/Runable.java
+++ src/main/java/HomeWork1/Runable.java
@@ -5,29 +5,4 @@ public interface Runable  {
     boolean run(LengthObstacle lengthObstacle);
 
 
-
-
-//    default void run(){
-//        System.out.println("пробежал");
-//    }
-//
-//    @Override
-//    default boolean overtake(Obstacle obstacle) {
-//        return run((LengthObstacle) obstacle);
-//    }
-//
-//    default boolean run(LengthObstacle lengthObstacle){
-//        if (validate(lengthObstacle) == true){
-//            System.out.println("Пробежал препятствие");
-//            return true;
-//        } else {
-//            System.out.println("Не пробежал препятствие");
-//            return false;
-//        }
-//    }
-//
-//    default boolean validate(LengthObstacle obstacle){
-//        return true;
-//    }
-
 }
